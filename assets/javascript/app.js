3bfbdc6f7b3adb5411397704c319e322ee9812a1
Bridget Bourne <Bridget Bourne>
2017-05-30 23:27:24 -0400

    pseudo coding basic variables and images

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+//REMEMBER TO CONSOLE.LOG EVERTHING! LEAVE NO STONE UNTURNED!
+
+//Step 1: Create your basic variables for your trivia game
+var question = [];
+var correctAnswer = [];
+var wrongAnswer = {
+/*correctAnswer.index0: [wronga1, wronga2, wronga3]
+			... .indexm: [wrongn1, wrongn2, wrongn3]*/
+};
+var correctGuess = function() {
+
+};
+var wrongGuess = function() {
+
+};
+var timeUp = function(){
+
+};
+
+//Step 2: Create click events which allow users to select and input guesses. Maybe you'll want a Submit button, but not required.
+//You will need to create classes or IDs to assign to these click events.
+$("img").on("click", function charSelect()) {
+
+};
+$("img").on("click", function charSelect()) {
+
+};
+
+/*Step 3: Create functions which cause users' guesses to change the screens. Change from question screen to Correct or Wrong screen.
+Make sure to change result screen over to next question.*/
+
+/*Step 4: Create timer which runs during each question running. 
+Then create a function which changes question screen to Time Up screen on condition that timer runs out.*/
+
+/*Step 5: Create function which displays final score screen after all questions are answered.
+Create a button which allows player to play again*/
+
+//Step 6: 
