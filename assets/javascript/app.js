7ff2fadcef1fba4cd46bf98578cdb1d005cd8534
Bridget Bourne <Bridget Bourne>
2017-06-02 09:52:34 -0400

    homework submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -74,62 +74,62 @@ jQuery(document).ready(function($){
 function(){
 	
 };
-// console.log();
-// };
-// var wrongGuess = function() {
-// console.log();
-// };
+console.log();
+};
+var wrongGuess = function() {
+console.log();
+};
 
-// var score = 0;
+var score = 0;
 
-// //Step 2: Create click events which allow users to select and input guesses. Maybe you'll want a Submit button, but not required.
-// //You will need to create classes or IDs to assign to these click events.
-// /*""$("correctGuess").on("click", function charSelect()) {
+//Step 2: Create click events which allow users to select and input guesses. Maybe you'll want a Submit button, but not required.
+//You will need to create classes or IDs to assign to these click events.
+ /*""$("correctGuess").on("click", function charSelect()) {
 
-// };
-// $("wrongGuess").on("click", function charSelect()) {
+ };
+ $("wrongGuess").on("click", function charSelect()) {
 
-// };""*/
+ };""*/
 
 // /*Step 3: Create functions which cause users' guesses to change the screens. Change from question screen to Correct or Wrong screen.
 // Make sure to change result screen over to next question.*/
-// var guess = $("question").on("click", function(){
-// 	if(correctAnswer) {
-// 		score += 1;
-// 		alert("Correct!");
-// 		document.//insert "Correct" screen here.
-// 		console.log(guess)
-// 	};
-// 	else{
-// 		alert("Incorect! The correct answer is " + correctAnswer ".");
-// 		document.//insert "Wrong" screen here.
-// 		console.log(guess)
-// 	};
-// });
+ var guess = $("question").on("click", function(){
+ 	if(correctAnswer) {
+ 		score += 1;
+ 		alert("Correct!");
+ 		document.//insert "Correct" screen here.
+ 		console.log(guess)
+ 	};
+ 	else{
+ 		alert("Incorect! The correct answer is " + correctAnswer ".");
+ 		document.//insert "Wrong" screen here.
+ 		console.log(guess)
+};
+});
 	
 
-// /*Step 4: Create timer which runs during each question running. 
-// Then create a function which changes question screen to Time Up screen on condition that timer runs out.*/
-// setTimeout(timeUp, 1000 * 30);
+//Step 4: Create timer which runs during each question running. 
+Then create a function which changes question screen to Time Up screen on condition that timer runs out.*/
+setTimeout(timeUp, 1000 * 30);
 
-// var timeUp = function(){
-// alert("The music has stopped. The correct answer is " + correctAnswer + ".");
-// console.log("Okay, I'm bored now. Blah blah blah!");
-// };
+var timeUp = function(){
+alert("The music has stopped. The correct answer is " + correctAnswer + ".");
+console.log("Okay, I'm bored now. Blah blah blah!");
+};
 
-// /*Step 5: Create function which displays final score screen after all questions are answered.
-// Create a button which allows player to play again*/
-// finalScore = function(){
-// $("<button>Encore!</button>");
-// alert("Your total score is " + score + ". Do you want to play again?");
-// document.___
-// console.log();
-// };
-// //Step 6: 
-// $(".target").hide();
-// $(".target").show();
-// $(".target").on("hover", function() {
-// 	$(".target").show();
-// });
-// /* */
+/*Step 5: Create function which displays final score screen after all questions are answered.
+Create a button which allows player to play again*/
+finalScore = function(){
+$("<button>Encore!</button>");
+alert("Your total score is " + score + ". Do you want to play again?");
+document.___
+console.log();
+};
+//Step 6: 
+$(".target").hide();
+$(".target").show();
+$(".target").on("hover", function() {
+$(".target").show();
+});
+
 });
